897bef4677a0b7a3d37a4c2a9ec60d832d31c84f
Jorge Loch <jorgembloch2001@gmail.com>
2024-08-01 21:26:46 -0300

    feat: adding multithread

--- unit_1/pwcracker/src/jorgeloch_202200014195_pwcracker.c
+++ unit_1/pwcracker/src/jorgeloch_202200014195_pwcracker.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdint.h>
+#include <pthread.h>
+#include <sys/sysinfo.h>
 
 // Macro rand8
 # define rand8 ((hrand() >> 8) & 0b111)
 # define pw(p, i, n) ((i < n) ? (p[i]) : (p[i%n]*i))
 
 // Número pseudo - aleatório
-static uint32_t ri = 1;
+static __thread uint32_t ri = 1;
 // Função para ajuste de semente
 void hsrand ( uint32_t seed ) 
 {
@@ -38,48 +40,76 @@ void MAU_64 (uint8_t* hash, const char* senha)
 }
 
 typedef struct user {
-  char login[9];
   uint64_t hash;
   uint32_t original_index;
-  char senha[5]; 
+  char login[9];
+  char senha[5];
 } user;
 
-static char possibilities[63] = "\0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+static char possibilities[63] = "uC6Z4gJhzfRLPtXoNMIW8ajcykx3EY7bqKOTH10wmiQABeS9FnlG2drvsp5DUV\0";
+static char *next_character = possibilities;
+static user* users;
+static uint32_t n_users;
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void crack_hashes(user* users, uint32_t n_users);
+void* cracker_behavior ();
+void crack_hashes();
 uint64_t hash_to_number(uint8_t *hash);
-int compare_users(const void* a, const void* b);
+int compare_users_by_hash(const void* a, const void* b);
+int compare_user_by_index(const void* a, const void* b);
 
 int main (int argc, char** argv)
 {
+  // garantindo número correto de argumentos
   if (argc != 3)
   {
     printf("usage: %s <input file> <output file>\n", argv[0]);
   }
+  // abrindo arquivos de entrada e saída
   FILE *input = fopen(argv[1], "r");
   FILE *output = fopen(argv[2], "w");
 
-  uint32_t n_users;
+  // lendo do arquivo de entrada o numero de usuarios a serem lidos
   fscanf(input, "%u ", &n_users);
 
-  user *users = (user*) calloc(n_users, sizeof(user));
+  // alocando espaço para os usuários
+  users = (user*) calloc(n_users, sizeof(user));
   for (int i = 0; i < n_users; i++)
   {
+    // lendo o login do usuário do arquivo de entrada
+    // regex para ler até 8 caracteres que não sejam ':'
     fscanf(input, "%8[^:]:", users[i].login);
+    // lendo o hash do usuário do arquivo de entrada como um inteiro de 64 bits
     fscanf(input, "%lx ", &users[i].hash);
     users[i].original_index = i;
   }
 
-  qsort(users, n_users, sizeof(user), compare_users);
+  // verifica o número de cores disponiveis na maquina
+  uint8_t n_threads = get_nprocs();
+  // cria array de threads
+  pthread_t crackers[n_threads];
 
-  crack_hashes(users, n_users);
-
-  for (int i = 0; i < n_users; i++)
+  for (int i = 0; i < n_threads; i++)
   {
-    printf("login: %s\n", users[i].login);
-    printf("senha: %s\n", users[i].senha);
+    // cria threads passando como parametro a rotina a ser executada
+    pthread_create(&crackers[i], NULL, cracker_behavior, NULL);
   }
 
+  // ordena array de usuários por hash para poder fazer busca binária
+  qsort(users, n_users, sizeof(user), compare_users_by_hash);
+  for (int i = 0; i < n_threads; i++)
+  {
+    pthread_join(crackers[i], NULL);
+  }
+
+  // ordena array de usuários por índice original para poder escrever no arquivo de saída
+  qsort(users, n_users, sizeof(user), compare_user_by_index);
+  for (int i = 0; i < n_users; i++)
+  {
+    fprintf(output, "%s:%s\n", users[i].login, users[i].senha);
+  }
+
+  // liberando memoria
   fclose(input);
   fclose(output);
   free(users);
@@ -96,7 +126,8 @@ uint64_t hash_to_number(uint8_t* array)
   return result;
 }
 
-int compare_users(const void* a, const void* b)
+// função para comparar usuários por hash
+int compare_users_by_hash(const void* a, const void* b)
 {
   user *user_a = (user*) a;
   user *user_b = (user*) b;
@@ -114,8 +145,38 @@ int compare_users(const void* a, const void* b)
   }
 }
 
-bool search_hash(user* users, uint32_t n_users, uint64_t target, uint32_t* index)
+// fnção para comparar usuários por índice original
+int compare_user_by_index(const void* a, const void* b)
 {
+  user *user_a = (user*) a;
+  user *user_b = (user*) b;
+  if (user_a->original_index > user_b->original_index)
+  {
+    return 1;
+  }
+  else if (user_b->original_index > user_a->original_index)
+  {
+    return -1;
+  }
+  else 
+  {
+    return 0;
+  }
+}
+
+// função para buscar hash no array de usuários
+bool search_hash(uint64_t target, uint32_t* index)
+{
+  // caso prefira utilizar busca sequencial
+  /* for (int i = 0; i < n_users; i++) */
+  /* { */
+  /*   if (users[i].hash == target) */
+  /*   { */
+  /*     *index = i; */
+  /*     return true; */
+  /*   } */
+  /* } */
+  // caso prefira utilizar busca binaria
   uint32_t left = 0, right = n_users,  middle;
   while (left < right)
   {
@@ -136,40 +197,55 @@ bool search_hash(user* users, uint32_t n_users, uint64_t target, uint32_t* index
   return false;
 }
 
-void crack_hashes(user* users, uint32_t n_users)
+// rotina a ser implementada pelas threads
+void* cracker_behavior ()
 {
-  uint32_t counter = 0;
-  uint8_t buffer[8];
-  for (int i = 0; i < 63; i++)
+  while (next_character)
   {
-    for (int j = i == 0 ? 0 : 1; j < 63; j++)
+    // iniciar mutex
+		pthread_mutex_lock(&mutex);
+    char cur_char = *next_character; 
+    next_character = cur_char == 'V' ? NULL : next_character + 1 * sizeof(char);
+    // liberar o mutex
+		pthread_mutex_unlock(&mutex);
+    uint8_t buffer[8];
+    // gerar permutações
+    for (int i = 63; i >= 0; i--)
     {
-      for (int k = 1; k < 63; k++)
+      // esse j dependendo do valor de i garante que todas as strings que tem 
+      // tamanho 2 não possuam caracteres a direita do primeiro \0
+      for (int j = i == 63 ? 63 : 62; j >= 0; j--)
       {
-        for (int l = 1; l < 63; l++)
+        for (int k = 0; k < 62; k++)
         {
-          char password[5] = {
+          char test_password[5] = {
+            cur_char,
             possibilities[k],
-            possibilities[l],
             possibilities[j],
             possibilities[i],
             '\0'
           };
 
-          MAU_64(buffer, password);
+          // aplica o hash MAU-64 a senha gerada
+          MAU_64(buffer, test_password);
+          // transforma o hash gerado em um numero de 64 bits
           uint64_t new_hash = hash_to_number(buffer);
-          
+
           uint32_t index;
-          if (search_hash(users, n_users, new_hash, &index))
+          // busca a existencia do hash na lista de usuários
+          if (search_hash(new_hash, &index))
           {
-            counter++;
-            strcpy(users[index].senha, password);
-            if (counter == n_users) return; 
+            // iniciar mutex
+            pthread_mutex_lock(&mutex);
+            // salva a senha do usuário na struct de usuário na posição correta 
+            strcpy(users[index].senha, test_password);
+            // liberar mutex
+            pthread_mutex_unlock(&mutex);
           }
         }
       }
     }
   }
-  return;
+  pthread_exit(NULL);
 }
 
